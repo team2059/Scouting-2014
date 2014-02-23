9c68349e7c6472a191b0dc796af3587305a4a78d
stonewareslord <stonewareslord@2946f01a-19ac-4363-aa36-349d2eb25ccf>
2014-02-23 23:23:10 +0000

    changed get to post in process.php
    git-svn-id: svn://10.0.1.200/hitchhikers/2014-Hitchhiker-Code/scouting@197 2946f01a-19ac-4363-aa36-349d2eb25ccf

--- process.php
+++ process.php
@@ -1,28 +1,26 @@
 <?php
 include('connection.php');
-
 $data=array();
-$data[':teamNumber']=$_GET['teamNumber'];
-$data[':matchNumber']=$_GET['matchNumber'];
-$data[':autohtMiss']=$_GET['autohtMiss'];
-$data[':autohtMade']=$_GET['autohtMade'];
-$data[':hotGoal']=$_GET['hotGoal'];
-$data[':autoltMiss']=$_GET['autoltMiss'];
-$data[':autoltMade']=$_GET['autoltMade'];
-$data[':hotZone']=$_GET['hotZone'];
-$data[':startPosition']=$_GET['startPosition'];
-$data[':htMiss']=$_GET['htMiss'];
-$data[':htMade']=$_GET['htMade'];
-$data[':ltMiss']=$_GET['ltMiss'];
-$data[':ltMade']=$_GET['ltMade'];
-$data[':passes']=$_GET['passes'];
-$data[':catches']=$_GET['catches'];
-$data[':truss']=$_GET['truss'];
-$data[':pointsPrevented']=$_GET['pointsPrevented'];
-$data[':note']=$_GET['note'];
+$data[':teamNumber']=$_POST['teamNumber'];
+$data[':matchNumber']=$_POST['matchNumber'];
+$data[':autohtMiss']=$_POST['autohtMiss'];
+$data[':autohtMade']=$_POST['autohtMade'];
+$data[':hotGoal']=$_POST['hotGoal'];
+$data[':autoltMiss']=$_POST['autoltMiss'];
+$data[':autoltMade']=$_POST['autoltMade'];
+$data[':hotZone']=$_POST['hotZone'];
+$data[':startPosition']=$_POST['startPosition'];
+$data[':htMiss']=$_POST['htMiss'];
+$data[':htMade']=$_POST['htMade'];
+$data[':ltMiss']=$_POST['ltMiss'];
+$data[':ltMade']=$_POST['ltMade'];
+$data[':passes']=$_POST['passes'];
+$data[':catches']=$_POST['catches'];
+$data[':truss']=$_POST['truss'];
+$data[':pointsPrevented']=$_POST['pointsPrevented'];
+$data[':note']=$_POST['note'];
 $data[':ip']=$_SERVER['REMOTE_ADDR'];
 $data[':mac']='';
-
 $ipAddress=$_SERVER['REMOTE_ADDR'];
 $arp="arp -a $ipAddress";
 $lines=explode("\n", $arp);
@@ -32,9 +30,7 @@ foreach($lines as $line) {
     $data[':mac']=$cols[1];
   }
 }
-
 $stmt=$dbh->prepare("INSERT INTO rounds (teamNumber,matchNumber,autohtMiss,autohtMade,hotGoal,autoltMiss,autoltMade,hotZone,startPosition,htMiss,htMade,ltMiss,ltMade,passes,catches,truss,pointsPrevented,note,ip,mac) VALUES (:teamNumber,:matchNumber,:autohtMiss,:autohtMade,:hotGoal,:autoltMiss,:autoltMade,:hotZone,:startPosition,:htMiss,:htMade,:ltMiss,:ltMade,:passes,:catches,:truss,:pointsPrevented,:note,:ip,:mac)");
-
 $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 try {
   $stmt->execute($data);
