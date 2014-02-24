69d2155b48af0f2f7af5d1c9ce3b413b8ea3ec3f
stonewareslord <stonewareslord@2946f01a-19ac-4363-aa36-349d2eb25ccf>
2014-02-24 22:15:16 +0000

    Fixed code in form.php
    git-svn-id: svn://10.0.1.200/hitchhikers/2014-Hitchhiker-Code/scouting@198 2946f01a-19ac-4363-aa36-349d2eb25ccf

--- connection.php
+++ connection.php
@@ -1,7 +1,4 @@
 <?php
-$hostname = "localhost";
-$username = "2059";
-$password = "r62h4yLSAEnTJ2K5";
 try {
   $dbh = new PDO("mysql:host=localhost;dbname=2059","2059","r62h4yLSAEnTJ2K5");
   echo("Connected to database");
--- form.php
+++ form.php
@@ -1,6 +1,45 @@
-<?php  session_start(); 
-     include('connection.php');
-     $db_query= mysql_query("SELECT * FROM test");
+<?php
+session_start(); 
+require('connection.php');
+//             WHAT DO YOU THINK YOU'RE DOING CHRISTIAN!!!!!!!!!!!
+//             HAVE YOU PROGRAMMED BEFORE!?!?!??!?!?!!!
+//             ALWAYS ADD LIMIT 1 FOR A TEST!!!
+//             EJFKEJKFJSKLJWELAFJAE;LAJKE;LGJA;LGJAEL;KJEA
+//             WTF!! mysql_query!?!?!?!?!?!?!?!?!?!!?!?!?!?!?!?!?!?!?
+//             NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+//
+//             Crucifix of bad code:
+//                              _________
+//                              |       |
+//          ____________________|       |___________________
+//          |                                              |
+//          | $db_query= mysql_query("SELECT * FROM test");|
+//          |                                              |
+//          |___________________        ___________________|
+//                              |       |
+//                              |       |
+//                              |       |
+//                              |       |
+//                              |       |
+//                              |       |
+//                              |       |
+//                              |       |
+//                              |       |
+//                              |       |
+//                              |       |
+//                              |       |
+//                              |_______|
+//Here's the right way: 
+//$test=$dbh->preare("SELECT * FROM rounds LIMIT 1");
+//try{
+//  $test->execute();
+//}catch(PDOException $e){
+//  //TODO remove this in prod
+//  die($e->getMessage());
+//  //die("Could not select database");
+//}
+//
+//But, it automatically does it in connection.php
 ?>
 <!DOCTYPE>
 <head>
@@ -10,25 +49,16 @@
 <script src="jmobile/jquery.mobile-1.4.1.js"></script>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 </head> 
-
 <body> 
-
 <div id="header">
 <div id="headerbutton"><a href="index.php">Home</a></div>
 <h1>Regional Menu</h1>
 </div>
-
 <div id="content">
-
-
-
-
 <form action="process.php" method="post" />
 <h1>Match Info</h1>
 <p>Team Number: <input type="number" name="teamNumber" /></p>
 <p>Match Number: <input type="number" name="matchNumber" /></p>
-
-
 <h1>Auto</h1>
 <p>First 10 seconds of the match</p>
 <table>
@@ -52,24 +82,20 @@
 </table>
 <label for="hotZone">Hot Zone:</label>
 <select name="hotZone" id="hotZone" data-role="slider">
-	<option value="no">No</option>
-        <option value="yes">Yes</option>
+  <option value="no">No</option>
+  <option value="yes">Yes</option>
 </select> 
-
 <fieldset data-role="controlgroup">
-	<legend>Start Position:</legend>
-     	<input type="radio" name="startPosition" id="left" value="left" />
-     	<label for="great">Left</label>
-     	<input type="radio" name="startPosition" id="middle" value="middle"  />
-     	<label for="average">Middle</label>
-     	<input type="radio" name="startPosition" id="right" value="right"  />
-     	<label for="no">Right</label>
-        <input type="radio" name="startPosition" id="goalie" value="goalie"  />
-     	<label for="no">Goalie</label>
+  <legend>Start Position:</legend>
+  <input type="radio" name="startPosition" id="left" value="left" />
+  <label for="great">Left</label>
+  <input type="radio" name="startPosition" id="middle" value="middle"  />
+  <label for="average">Middle</label>
+  <input type="radio" name="startPosition" id="right" value="right"  />
+  <label for="no">Right</label>
+  <input type="radio" name="startPosition" id="goalie" value="goalie"  />
+  <label for="no">Goalie</label>
 </fieldset>
-
-
-
 <h1>Teleop</h1>
 <p>Driver control period</p>
 <table>
@@ -100,12 +126,10 @@
 <td><input type="button" name="increase" value="+" onclick="increasehb()"/></td></tr>
 </tr>
 </table>
-
 <p>Comments: <input type="text" name="notes" /></p>
 <input type="submit" value="Submit" />
 </form>
-	</div>
-
-
+  </div>
 </body>
 </html>
+<?php
