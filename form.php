20d1fef42efdd2e8ccbba98f7bf2e6b1f3d6cc93
christian <christian@2946f01a-19ac-4363-aa36-349d2eb25ccf>
2014-02-26 01:05:56 +0000

    Hardcoded on hardmode
    git-svn-id: svn://10.0.1.200/hitchhikers/2014-Hitchhiker-Code/scouting@202 2946f01a-19ac-4363-aa36-349d2eb25ccf

--- form.php
+++ form.php
@@ -9,17 +9,33 @@ require('connection.php');
 <script src="js/jquery.js"></script>
 <script src="js/jquerymobile.js"></script>
 <script>
-function increaseht() {
-    document.getElementById("autohtMiss").value = Number(document.getElementById("autohtMiss").value) + 1;
-}
-function decreaseht() {
-	document.getElementById("autohtMiss").value = Number(document.getElementById("autohtMiss").value) - 1;
-}
-$("input[:type=button]").onClick({
-	alert('a');
-	
-	$(this).parent().find("input[:type=text]").first().html(1+parseInt($(this).parent().first().html()));
-});
+function autohtMissp() {document.getElementById("autohtMiss").value = Number(document.getElementById("autohtMiss").value) + 1;}
+function autohtMissm() {document.getElementById("autohtMiss").value = Number(document.getElementById("autohtMiss").value) - 1;}
+function autohtMadep() {document.getElementById("autohtMade").value = Number(document.getElementById("autohtMade").value) + 1;}
+function autohtMadem() {document.getElementById("autohtMade").value = Number(document.getElementById("autohtMade").value) - 1;}
+function hotGoalp() {document.getElementById("hotGoal").value = Number(document.getElementById("hotGoal").value) + 1;}
+function hotGoalm() {document.getElementById("hotGoal").value = Number(document.getElementById("hotGoal").value) - 1;}
+function autoltMissp() {document.getElementById("autoltMiss").value = Number(document.getElementById("autoltMiss").value) + 1;}
+function autoltMissm() {document.getElementById("autoltMiss").value = Number(document.getElementById("autoltMiss").value) - 1;}
+function autoltMadep() {document.getElementById("autoltMade").value = Number(document.getElementById("autoltMade").value) + 1;}
+function autoltMadem() {document.getElementById("autoltMade").value = Number(document.getElementById("autoltMade").value) - 1;}
+
+function htMissp() {document.getElementById("htMiss").value = Number(document.getElementById("htMiss").value) + 1;}
+function htMissm() {document.getElementById("htMiss").value = Number(document.getElementById("htMiss").value) - 1;}
+function htMadep() {document.getElementById("htMade").value = Number(document.getElementById("htMade").value) + 1;}
+function htMadem() {document.getElementById("htMade").value = Number(document.getElementById("htMade").value) - 1;}
+function ltMissp() {document.getElementById("ltMiss").value = Number(document.getElementById("ltMiss").value) + 1;}
+function ltMissm() {document.getElementById("ltMiss").value = Number(document.getElementById("ltMiss").value) - 1;}
+function ltMadep() {document.getElementById("ltMade").value = Number(document.getElementById("ltMade").value) + 1;}
+function ltMadem() {document.getElementById("ltMade").value = Number(document.getElementById("ltMade").value) - 1;}
+function passesp() {document.getElementById("passes").value = Number(document.getElementById("passes").value) + 1;}
+function passesm() {document.getElementById("passes").value = Number(document.getElementById("passes").value) - 1;}
+function catchesp() {document.getElementById("catches").value = Number(document.getElementById("catches").value) + 1;}
+function catchesm() {document.getElementById("catches").value = Number(document.getElementById("catches").value) - 1;}
+function trussp() {document.getElementById("truss").value = Number(document.getElementById("truss").value) + 1;}
+function trussm() {document.getElementById("truss").value = Number(document.getElementById("truss").value) - 1;}
+function pointsPreventedp() {document.getElementById("pointsPrevented").value = Number(document.getElementById("pointsPrevented").value) + 1;}
+function pointsPreventedm() {document.getElementById("pointsPrevented").value = Number(document.getElementById("pointsPrevented").value) - 1;}
 </script>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 </head> 
@@ -38,20 +54,20 @@ $("input[:type=button]").onClick({
 <table>
 <tr>
 <td><label for="autohtMiss">High Target Missed: </label></td>
-<td><input type="button" name="decrease" value="-" onclick="decreaseht()"/></td><td><input type="text" name="autohtMiss" value="0" id="autohtMiss"/></td>
-<td><input type="button" name="increase" value="+" onclick="increaseht()"/></td></tr>
+<td><input type="button" name="decrease" value="-" onclick="autohtMissm()"/></td><td><input type="text" name="autohtMiss" value="0" id="autohtMiss"/></td>
+<td><input type="button" name="increase" value="+" onclick="autohtMissp()"/></td></tr>
 <tr><td><label for="autohtMade">High Target Made: </label></td>
-<td><input type="button" name="decrease" value="-" onclick="decreasehm()"/></td><td><input type="text" name="autohtMade" value="0" id="autohtMade"/></td>
-<td><input type="button" name="increase" value="+" onclick="increasehm()"/></td></tr>
+<td><input type="button" name="decrease" value="-" onclick="autohtMadem()"/></td><td><input type="text" name="autohtMade" value="0" id="autohtMade"/></td>
+<td><input type="button" name="increase" value="+" onclick="autohtMadep()"/></td></tr>
 <tr><td><label for="hotGoal">Hot Goal: </label></td>
-<td><input type="button" name="decrease" value="-" onclick="decreasehb()"/></td><td><input type="text" name="hotGoal" value="0" id="hotGoal"/></td>
-<td><input type="button" name="increase" value="+" onclick="increasehb()"/></td></tr>
+<td><input type="button" name="decrease" value="-" onclick="hotGoalm()"/></td><td><input type="text" name="hotGoal" value="0" id="hotGoal"/></td>
+<td><input type="button" name="increase" value="+" onclick="hotGoalp()"/></td></tr>
 <tr><td><label for="autoltMiss">Low Target Missed: </label></td>
-<td><input type="button" name="decrease" value="-" onclick="decreasehb()"/></td><td><input type="text" name="autoltMiss" value="0" id="autoltMiss"/></td>
-<td><input type="button" name="increase" value="+" onclick="increasehb()"/></td></tr>
+<td><input type="button" name="decrease" value="-" onclick="autoltMissm()"/></td><td><input type="text" name="autoltMiss" value="0" id="autoltMiss"/></td>
+<td><input type="button" name="increase" value="+" onclick="autoltMissp()"/></td></tr>
 <tr><td><label for="autoltMade">Low Target Made: </label></td>
-<td><input type="button" name="decrease" value="-" onclick="decreasehb()"/></td><td><input type="text" name="autoltMade" value="0" id="autoltMade"/></td>
-<td><input type="button" name="increase" value="+" onclick="increasehb()"/></td></tr>
+<td><input type="button" name="decrease" value="-" onclick="autoltMadem()"/></td><td><input type="text" name="autoltMade" value="0" id="autoltMade"/></td>
+<td><input type="button" name="increase" value="+" onclick="autoltMadep()"/></td></tr>
 </tr>
 </table>
 <label for="hotZone">Hot Zone:</label>
@@ -87,29 +103,29 @@ $("input[:type=button]").onClick({
 <table>
 <tr>
 <td><label for="htMiss">High Target Missed: </label></td>
-<td><input type="button" name="decrease" value="-" onclick="decreaseht()"/></td><td><input type="text" name="htMiss" value="0" id="htMiss"/></td>
-<td><input type="button" name="increase" value="+" onclick="increaseht()"/></td></tr>
+<td><input type="button" name="decrease" value="-" onclick="htMissm()"/></td><td><input type="text" name="htMiss" value="0" id="htMiss"/></td>
+<td><input type="button" name="increase" value="+" onclick="htMissp()"/></td></tr>
 <tr><td><label for="htMade">High Target Made: </label></td>
-<td><input type="button" name="decrease" value="-" onclick="decreasehm()"/></td><td><input type="text" name="htMade" value="0" id="htMade"/></td>
-<td><input type="button" name="increase" value="+" onclick="increasehm()"/></td></tr>
+<td><input type="button" name="decrease" value="-" onclick="htMadem()"/></td><td><input type="text" name="htMade" value="0" id="htMade"/></td>
+<td><input type="button" name="increase" value="+" onclick="htMadep()"/></td></tr>
 <tr><td><label for="ltMiss">Low Target Missed: </label></td>
-<td><input type="button" name="decrease" value="-" onclick="decreasehb()"/></td><td><input type="text" name="ltMiss" value="0" id="ltMiss"/></td>
-<td><input type="button" name="increase" value="+" onclick="increasehb()"/></td></tr>
+<td><input type="button" name="decrease" value="-" onclick="ltMissm()"/></td><td><input type="text" name="ltMiss" value="0" id="ltMiss"/></td>
+<td><input type="button" name="increase" value="+" onclick="ltMissp()"/></td></tr>
 <tr><td><label for="ltMade">Low Target Made: </label></td>
-<td><input type="button" name="decrease" value="-" onclick="decreasehb()"/></td><td><input type="text" name="ltMade" value="0" id="ltMade"/></td>
-<td><input type="button" name="increase" value="+" onclick="increasehb()"/></td></tr>
+<td><input type="button" name="decrease" value="-" onclick="ltMadem()"/></td><td><input type="text" name="ltMade" value="0" id="ltMade"/></td>
+<td><input type="button" name="increase" value="+" onclick="ltMadep()"/></td></tr>
 <tr><td><label for="passes">Passes: </label></td>
-<td><input type="button" name="decrease" value="-" onclick="decreasehb()"/></td><td><input type="text" name="passes" value="0" id="passes"/></td>
-<td><input type="button" name="increase" value="+" onclick="increasehb()"/></td></tr>
+<td><input type="button" name="decrease" value="-" onclick="passesm()"/></td><td><input type="text" name="passes" value="0" id="passes"/></td>
+<td><input type="button" name="increase" value="+" onclick="passesp()"/></td></tr>
 <tr><td><label for="catches">Catches: </label></td>
-<td><input type="button" name="decrease" value="-" onclick="decreasehb()"/></td><td><input type="text" name="catches" value="0" id="catches"/></td>
-<td><input type="button" name="increase" value="+" onclick="increasehb()"/></td></tr>
+<td><input type="button" name="decrease" value="-" onclick="catchesm()"/></td><td><input type="text" name="catches" value="0" id="catches"/></td>
+<td><input type="button" name="increase" value="+" onclick="catchesp()"/></td></tr>
 <tr><td><label for="truss">Shots over truss: </label></td>
-<td><input type="button" name="decrease" value="-" onclick="decreasehb()"/></td><td><input type="text" name="truss" value="0" id="truss"/></td>
-<td><input type="button" name="increase" value="+" onclick="increasehb()"/></td></tr>
+<td><input type="button" name="decrease" value="-" onclick="trussm()"/></td><td><input type="text" name="truss" value="0" id="truss"/></td>
+<td><input type="button" name="increase" value="+" onclick="trussp()"/></td></tr>
 <tr><td><label for="pointsPrevented">Estimate Points Prevented: </label></td>
-<td><input type="button" name="decrease" value="-" onclick="decreasehb()"/></td><td><input type="text" name="pointsPrevented" value="0" id="pointsPrevented"/></td>
-<td><input type="button" name="increase" value="+" onclick="increasehb()"/></td></tr>
+<td><input type="button" name="decrease" value="-" onclick="pointsPreventedm()"/></td><td><input type="text" name="pointsPrevented" value="0" id="pointsPrevented"/></td>
+<td><input type="button" name="increase" value="+" onclick="pointsPreventedp()"/></td></tr>
 </tr>
 </table>
 <p>Comments: <input type="text" name="notes" /></p>
